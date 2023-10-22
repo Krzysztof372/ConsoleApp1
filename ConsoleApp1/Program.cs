75668f88c0b555a0947405078950981647eac372
Krzysztof <Krzysztof@DESKTOP-EJ7O61F>
2023-10-22 12:51:26 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        List<Employee> employees = new List<Employee>();
+
+        
+        Employee User1 = new Employee("Adam Mickiewicz", 30);
+        Employee User2 = new Employee("Julian Tuwim", 42);
+        Employee User3 = new Employee("Mikołaj Kopernik", 55);
+
+        User1.AddRating(7);
+        User1.AddRating(9);
+        User1.AddRating(8);
+        User1.AddRating(6);
+        User1.AddRating(10);
+
+        User2.AddRating(5);
+        User2.AddRating(7);
+        User2.AddRating(8);
+        User2.AddRating(9);
+        User2.AddRating(6);
+
+        User3.AddRating(10);
+        User3.AddRating(9);
+        User3.AddRating(8);
+        User3.AddRating(7);
+        User3.AddRating(6);
+
+        employees.Add(User1);
+        employees.Add(User2);
+        employees.Add(User3);
+
+       
+        Employee employeeWithHighestRating = null;
+        int maxRating = 0;
+
+        foreach (Employee employee in employees)
+        {
+            if (employee.GetAverageRating() > maxRating)
+            {
+                maxRating = employee.GetAverageRating();
+                employeeWithHighestRating = employee;
+            }
+        }
+
+       
+        if (employeeWithHighestRating != null)
+        {
+            Console.WriteLine("Pracownik z najwyższą liczbą ocen:");
+            Console.WriteLine("Imię i nazwisko: " + employeeWithHighestRating.FullName);
+            Console.WriteLine("Wiek: " + employeeWithHighestRating.Age);
+            Console.WriteLine("Średnia ocen: " + employeeWithHighestRating.GetAverageRating());
+        }
+    }
+}
---
+++ ConsoleApp1/User.cs
@@ -0,0 +1,32 @@
+﻿
+class Employee
+{
+    public string FullName { get; set; }
+    public int Age { get; set; }
+    private List<int> ratings;
+
+    public Employee(string fullName, int age)
+    {
+        FullName = fullName;
+        Age = age;
+        ratings = new List<int>();
+    }
+
+    public void AddRating(int rating)
+    {
+        ratings.Add(rating);
+    }
+
+    public int GetAverageRating()
+    {
+        int sum = 0;
+        foreach (int rating in ratings)
+        {
+            sum += rating;
+        }
+        return sum / ratings.Count;
+
+
+    }
+}
+    
