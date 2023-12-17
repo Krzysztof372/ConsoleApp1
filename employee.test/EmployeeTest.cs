5b9c655a69baae81da254105502929cd8c651c89
Krzysztof <Krzysztof@DESKTOP-EJ7O61F>
2023-12-17 12:03:14 +0100

    Test

---
+++ employee.test/EmployeeTest.cs
@@ -0,0 +1,135 @@
+using ChallengeApp21;
+
+namespace challenge.tests;
+
+public class TypeTest
+
+
+{
+
+
+
+
+
+
+
+    [Test]
+
+    public void MinTest()
+    {
+
+
+        var employee = new Employee("Krzysztof Pietrzak");
+
+        employee.AddGrade(5);
+        employee.AddGrade(6);
+        employee.AddGrade(8);
+
+        var statistics = employee.GetStatistics();
+
+        Assert.AreEqual(5, statistics.Min);
+
+
+       
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+    [Test]
+    public void MaxTest()
+    {
+
+
+        var employee = new Employee("Krzysztof Pietrzak");
+
+        employee.AddGrade(10);
+        employee.AddGrade(6);
+        employee.AddGrade(7);
+
+        var statistics = employee.GetStatistics();
+
+        Assert.AreEqual(10, statistics.Max);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+    [Test]
+    public void AverageTest()
+    {
+
+
+        var employee = new Employee("Krzysztof Pietrzak");
+
+        employee.AddGrade(5);
+        employee.AddGrade(3);
+        employee.AddGrade(10);
+
+        var statistics = employee.GetStatistics();
+
+        Assert.AreEqual(6, statistics.Average);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    
