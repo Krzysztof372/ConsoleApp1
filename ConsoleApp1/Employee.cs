d4bdf1fff2a61da08d2faf25372aebecbb33d66b
Krzysztof <Krzysztof@DESKTOP-EJ7O61F>
2023-12-31 17:21:27 +0100

    konwertowanie

--- ConsoleApp1/Employee.cs
+++ ConsoleApp1/Employee.cs
@@ -1,61 +1,145 @@
 ﻿
 
+
+
+using ChallengeApp21;
+
 namespace ChallengeApp21
+{ }
+public class Employee
+
 {
-    public class Employee
+
+    private List<float> grades = new List<float>();
+
+    public Employee(string name)
+    {
+        this.Name = name;
+
+    }
+
+    public Employee(string name, string surname)
+    {
+        this.Name = name;
+        this.Surname = surname;
+    }
+
+    public string Name { get; private set; }
+
+    public string Surname { get; private set; }
+
+
+    public void AddGrade(float grade)
+
+    {
+        if (grade >= 0 && grade <= 100)
+
+
+        {
+
+            this.grades.Add(grade);
+
+        }
+
+        else
+
+        {
+
+            Console.WriteLine("Invalid code value");
+        }
+
+
+    }
+    public void AddGrade( string grade)
 
     {
 
-        private List<float> grades = new List<float>();
+        if
 
-        public Employee(string name)
+            (float.TryParse(grade, out float result))
+
         {
-            this.Name = name;
-            
+
+            this.AddGrade(result);
         }
 
-        public Employee(string name, string surname)
+        else
+
         {
-            this.Name = name;
-            this.Surname = surname;
+            Console.WriteLine("String is not float");
+
         }
-
-        public string Name { get; private set; }
-
-        public string? Surname { get;  private set; }
-
-
-        public void AddGrade(float grade)
-
-        { this.grades.Add(grade); }
-
-       
-
-        public Statistics GetStatistics()
-            {
-            var statistics = new Statistics();
-            statistics.Min = float.MaxValue;
-            statistics.Max = float.MinValue;
-            statistics.Average = 0;
-
-
-            foreach (var grade in this.grades)
-            {
-                statistics.Max = Math.Max(statistics.Max, grade);
-                statistics.Average += grade;
-                statistics.Min = Math.Min(statistics.Min, grade);
-            }
-
-
-            statistics.Average /= this.grades.Count;
-
-            return statistics;
-            }
-            
-
-         
-   
-
-            
     }
+
+    public void AddGrade(int grade)
+    {
+        if (grade < 0 && grade > 100)
+
+        {
+
+            this.grades.Add(grade);
+
+
+        }
+        else
+
+        {
+
+
+            Console.WriteLine("Int under the 100");
+
+
+
+        }
+    }
+
+    public void AddGrade(double grade, int v)
+
+    {
+
+        double doubleValue;
+
+
+        if (double.TryParse(float.MinValue.ToString(), out doubleValue))
+        {
+
+            Console.WriteLine("Wartość doubleValue wynosi: " + doubleValue);
+        }
+        else
+        {
+           
+            Console.WriteLine("Nie udało się sparsować wartości float.");
+        }
+
+
+
+    }
+
+    public Statistics GetStatistics()
+    {
+        var statistics = new Statistics();
+        statistics.Min = float.MaxValue;
+        statistics.Max = float.MinValue;
+        statistics.Average = 0;
+
+
+        foreach (var grade in this.grades)
+        {
+            statistics.Max = Math.Max(statistics.Max, grade);
+            statistics.Average += grade;
+            statistics.Min = Math.Min(statistics.Min, grade);
+        }
+
+
+        statistics.Average /= this.grades.Count;
+
+        return statistics;
+    }
+
 }
+
+
+
+
+
+
