fbe69f176fd32ab51bd3fdb21e30169ca71137e2
Krzysztof <Krzysztof@DESKTOP-EJ7O61F>
2024-01-08 21:16:25 +0100

    Dzień 11

--- ConsoleApp1/Employee.cs
+++ ConsoleApp1/Employee.cs
@@ -10,11 +10,7 @@ public class Employee
 {
     private List<float> grades = new List<float>();
 
-    public Employee(string name)
-    {
-        this.Name = name;
-
-    }
+   
     public Employee(string name, string surname)
     {
         this.Name = name;
@@ -39,38 +35,10 @@ public class Employee
             Console.WriteLine("Invalid code value");
         }
     }
-    public void AddGrade( string grade)
-    {
-        if
 
-            (float.TryParse(grade, out float result))
-        {
-            this.AddGrade(result);
-        }
-        else
-        {
-            Console.WriteLine("String is not float");
 
-        }
-    }
 
-    public void AddGrade(int grade)
-    {
-        if (grade < 0 && grade > 100)
-
-        {
-            this.grades.Add(grade);
-
-        }
-
-        else
-        {
-            Console.WriteLine("Int under the 100");
-
-        }
-    }
-
-    public void AddGrade(double grade, int v)
+    public void AddGrade(string grade)
 
     {
 
@@ -84,14 +52,17 @@ public class Employee
         }
         else
         {
-           
-            Console.WriteLine("Nie udało się sparsować wartości float.");
+
+            Console.WriteLine($"({grade})jest ");
         }
 
 
 
+
+
     }
-    public Statistics GetStatistics()
+
+    public Statistics GetStatisticsForeach()
     {
         var statistics = new Statistics();
         statistics.Min = float.MaxValue;
@@ -109,8 +80,81 @@ public class Employee
         return statistics;
     }
 
-}
+    public Statistics GetStatisticsFor()
+    {
+        var statistics = new Statistics();
+        statistics.Min = float.MaxValue;
+        statistics.Max = float.MinValue;
+        statistics.Average = 0;
+        var index = 0;
+        index = grades.Count;
+        index++;
 
+
+        for (index = 0; index < grades.Count; index++)
+        {
+            statistics.Max = Math.Max(statistics.Max, this.grades[index]);
+            statistics.Average += this.grades[index];
+            statistics.Min = Math.Min(statistics.Min, this.grades[index]);
+            index++;
+
+        }
+
+        statistics.Average /= this.grades.Count;
+
+
+
+        return statistics;
+    }
+
+    public Statistics GetStatisticsdoWhile()
+    {
+        var statistics = new Statistics();
+        statistics.Min = float.MaxValue;
+        statistics.Max = float.MinValue;
+        statistics.Average = 0;
+        var index = 0;
+        do
+        {
+            statistics.Max = Math.Max(statistics.Max, this.grades[index]);
+            statistics.Average += this.grades[index];
+            statistics.Min = Math.Min(statistics.Min, this.grades[index]);
+            index++;
+        }
+
+        while (index < this.grades.Count);
+
+
+        statistics.Average /= this.grades.Count;
+
+        return statistics;
+    }
+
+    public Statistics GetStatisticsWhile()
+    {
+        var statistics = new Statistics();
+        statistics.Min = float.MaxValue;
+        statistics.Max = float.MinValue;
+        statistics.Average = 0;
+        var index = 0;
+
+      
+        {
+            statistics.Max = Math.Max(statistics.Max, this.grades[index]);
+            statistics.Average += this.grades[index];
+            statistics.Min = Math.Min(statistics.Min, this.grades[index]);
+            index++;
+        }
+
+
+
+
+        statistics.Average /= this.grades.Count;
+
+        return statistics;
+    }
+
+}
 
 
 
