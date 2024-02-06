8b2b5ce429e09c535d23e215f529860e369b0113
Krzysztof <Krzysztof@DESKTOP-EJ7O61F>
2024-02-06 17:33:10 +0100

    dzien 12

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,17 +1,33 @@
 ﻿using ChallengeApp21;
-using System;
-
-var Employee = new Employee("Krzysztof", "Pietrzak");
-
-Employee.AddGrade(2);
-Employee.AddGrade(2);
-
-Employee.AddGrade(6);
 
 
+Console.WriteLine("Witamy w Programie XYZ Pracowników");
+Console.WriteLine("===================================");
+Console.WriteLine("Oceny Od 0 Do 100"); 
 
-var statistics = Employee.GetStatisticsForeach();
+var employee = new Employee();
 
+while (true)
+
+{
+    Console.WriteLine("Podaj ocenę pracownika");
+    var input = Console.ReadLine();
+    if (input == "q") 
+
+    {
+        break;
+    }
+
+    employee.AddGrade(input);
+}
+
+
+
+
+var statistics = employee.GetStatistics();
+Console.WriteLine($"Average: {statistics.Average}");
+Console.WriteLine($"Max: {statistics.Max}");
+Console.WriteLine($"Min: {statistics.Min}");
 
 
 
